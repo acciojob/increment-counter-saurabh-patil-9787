65581565c0c74953fc1a4a8fc84606ce2316d291
saurabh-patil-9787 <154727373+saurabh-patil-9787@users.noreply.github.com>
2025-03-10 12:25:47 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,17 @@
 //your JS code here. If required.
+const displayCounterValue = document.getElementById('counter');
+const incrementBtn = document.getElementById('incrementBtn');
+
+
+
+let counter = 0;
+incrementBtn.addEventListener('click',()=>{
+	// let counter = 0;
+    alert(counter)
+    counter++
+    displayCounterValue.textContent = counter
+
+    
+
+});
+
